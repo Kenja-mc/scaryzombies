f5f5d7157c947b0b85d06b4051078ace6eaeb31b
Kenja-mc <a3delta@gmail.com>
2021-05-19 18:42:27 -0500

    Creation of mod and configuration of gradle.properies

---
+++ src/main/java/net/scaryzombies/ScaryZombiesInit.java
@@ -0,0 +1,10 @@
+package net.scaryzombies;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ScaryZombiesInit implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Yo.");
+    }
+}
