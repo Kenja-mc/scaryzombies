7dc06f3f50b167ba3c2d18f29733b1986be6d86d
Kenja-mc <A3delta@gmail.com>
2021-12-10 20:38:51 -0600

    Added a build.yml file from fabric-example-mod

--- src/main/java/net/scaryzombies/PlayerDetectHandler.java
+++
@@ -1,6 +0,0 @@
-package net.scaryzombies;
-
-public class PlayerDetectHandler {
-    // Toggle
-//    public void crouchEvent(Player)
-}
--- src/main/java/net/scaryzombies/entity/mob/ScaryZombie/ScaryZombieRenderer.java
+++ src/main/java/net/scaryzombies/entity/mob/ScaryZombie/ScaryZombieRenderer.java
@@ -2,20 +2,18 @@ package net.scaryzombies.entity.mob.ScaryZombie;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.render.entity.EntityRendererFactory.Context;
+import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.ZombieEntityRenderer;
-import net.minecraft.client.render.entity.model.EntityModelLayers;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
 public class ScaryZombieRenderer extends ZombieEntityRenderer {
-    private static final Identifier TEXTURE = new Identifier("minecraft", "textures/entity/zombie/zombie.png");
+    private static final Identifier TEXTURE = new Identifier("scaryzombies", "textures/mobs/zombie.png");
 
-    public ScaryZombieRenderer(Context context) {
-        // Just yoinking vanilla EMLs for now
-        super(context, EntityModelLayers.ZOMBIE, EntityModelLayers.ZOMBIE_INNER_ARMOR, EntityModelLayers.ZOMBIE_OUTER_ARMOR);
+    public ScaryZombieRenderer(EntityRendererFactory.Context context) {
+        super(context);
     }
 
     protected void scale(ZombieEntity zombieEntity, MatrixStack matrixStack, float f) {
@@ -26,11 +24,6 @@ public class ScaryZombieRenderer extends ZombieEntityRenderer {
 
     public Identifier getTexture(ScaryZombieEntity entity) {
         return TEXTURE;
-    }
-
-    public static void init() {
-        //Register Model
-//        EntityModelLayerRegistry.registerModelLayer(SZE_MODEL_LAYER, TexturedModelData.of());
     }
 
 }
